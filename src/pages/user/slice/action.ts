a08c50cafe5aa4394c8ce55d77aa6ff6eb0282e0
bhakti0699 <poojalalcheta@gmail.com>
2023-08-20 18:38:06 +0530

    integrate user all apis

---
+++ src/pages/user/slice/action.ts
@@ -0,0 +1,75 @@
+import { Dispatch } from '@reduxjs/toolkit';
+// utils
+import axios from 'src/utils/axios';
+
+import { 
+CREATE_USER,
+DELETE_USER,
+EDIT_USER,
+VIEW_USER,
+GET_ALL_USERS
+} from './action_type';
+
+import { slice } from '.';
+export function getAllUsers() {
+    return async (dispatch: Dispatch) => {
+      dispatch(slice.actions.startLoading());
+      try {
+        const response = await axios.post(GET_ALL_USERS);
+        dispatch(slice.actions.getUserRecords(response.data.events));
+      } catch (error) {
+        dispatch(slice.actions.hasError(error));
+      }
+    };
+  }
+
+  export function createUser(userCreate: any) {
+    return async (dispatch: Dispatch) => {
+      try {
+        const response = await axios.post(CREATE_USER, userCreate);
+        dispatch(slice.actions.createUser(response.data));
+      } catch (error) {
+        dispatch(slice.actions.createUserError(error));
+      }
+    };
+  }
+
+  export function updateUserById(id: string, name: string) {
+    return async (dispatch: Dispatch) => {
+      try {
+        const response = await axios.post(EDIT_USER, {
+            UserId:id,
+            FirstName: name, 
+        });
+        dispatch(slice.actions.updateUser(response.data));
+      } catch (error) {
+        dispatch(slice.actions.updateUserError(error));
+      }
+    };
+  }
+
+  export function deleteUserById(id: string) {
+    return async (dispatch: Dispatch) => {
+      try {
+        const response = await axios.post(DELETE_USER, {
+            userId: id
+        });
+        dispatch(slice.actions.deleteUser(response.data));
+      } catch (error) {
+        dispatch(slice.actions.deleteUserError(error));
+      }
+    };
+  }
+
+  export function viewUserById(id: string) {
+    return async (dispatch: Dispatch) => {
+      try {
+        const response = await axios.post(VIEW_USER, {
+            userId: id
+        });
+        dispatch(slice.actions.viewUser(response.data));
+      } catch (error) {
+        dispatch(slice.actions.viewUserError(error));
+      }
+    };
+  }
---
+++ src/pages/user/slice/index.ts
@@ -0,0 +1,96 @@
+import { createSlice, Dispatch } from '@reduxjs/toolkit';
+import { IUserState } from './types';
+
+const initialState: IUserState = {
+  isUserLoading: false,
+  users: {
+    count: 0,
+    rows: [
+        {
+            Address1: '',
+            DateOfBirth: '',
+            DateOfJoin: '',
+            FirstName: '',
+            LastName: '',
+            MiddleName: '',
+            Mobile: '',
+            UserCode: '',
+            UserEmail: '',
+            UserGender: '',
+            UserId: '',
+            UserPassword: '',
+            UserRoleId: '',
+            UserStatus: false,
+            UserTypeCode: '',
+        },
+    ],
+  },
+  viewError: false,
+  viewSuccess: false,
+  error: {},
+  createUserError: false,
+  createUserMsg: '',
+  createUserSucess: false,
+  updateUserError: false,
+  updateUserMsg: '',
+  updateUserSuccess: false,
+  deleteUserError: false,
+  deleteUserMsg: '',
+  deleteUserSuccess: false,
+  viewUserData: {},
+  viewUserLoading: true,
+};
+
+const slice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    startLoading(state) {
+        state.isUserLoading = true;
+      },
+      getUserRecords(state, action) {
+        state.viewSuccess = true
+        state.isUserLoading = false;
+        state.users = action.payload.data.data;
+      },
+      hasError(state, action) {
+        state.isUserLoading = false;
+        state.error = action.payload;
+      },
+      createUser(state, action) {
+        state.createUserSucess = true
+        state.createUserMsg = action.payload.data.message;
+      },
+      createUserError(state, action) {
+        state.createUserError = true
+        state.createUserMsg = action.payload.data.message;
+      },
+      updateUser(state, action) {
+        state.updateUserSuccess = true
+        state.updateUserMsg = action.payload.data.message;
+      },
+      updateUserError(state, action) {
+        state.updateUserError = true
+        state.updateUserMsg = action.payload.data.message;
+      },
+      deleteUser(state, action) {
+        state.deleteUserSuccess = true
+        state.deleteUserMsg = action.payload.data.message;
+      },
+      deleteUserError(state, action) {
+        state.deleteUserError = true
+        state.deleteUserMsg = action.payload.data.message;
+      },
+      viewUser(state, action) {
+        state.viewUserLoading = false
+        state.viewUserData = action.payload.data.data;
+      },
+      viewUserError(state, action) {
+        state.viewUserLoading = false
+      },
+  }
+});
+
+// Reducer
+export { slice };
+export default slice.reducer;
