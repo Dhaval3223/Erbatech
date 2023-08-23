5ef7da741243bc5860df3fadb0b910c11e9b8ed3
Dhaval3223 <68138664+Dhaval3223@users.noreply.github.com>
2023-08-23 22:20:11 +0530

    added common table skeleton

---
+++ src/components/table-skeleton/index.tsx
@@ -0,0 +1,19 @@
+import { TableRow, TableCell, Skeleton } from "@mui/material";
+
+const TableSkeleton = ({
+  colums,
+}: {
+  colums: number;
+}) => (
+    <>
+    {[1, 2, 3, 4, 5].map((row) => (
+      <TableRow hover>
+        {Array(3).fill('_').map((item) => (
+        <TableCell>
+          <Skeleton />
+        </TableCell>))}
+      </TableRow>))}
+    </>             
+)
+
+export default TableSkeleton;
